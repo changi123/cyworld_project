250ae8557dc50b9f33361bac73fafcfddb22e0ba
hcghcg17 <hcghcg17@gmail.com>
2024-04-12 13:32:17 +0900

    Feat : 폴더구조 변경

--- src/main/java/com/changi/record_project/mapper/SampleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.changi.record_project.mapper;
-
-public interface SampleMapper {
-	
-	String getTime();
-
-}
--- src/main/java/com/changi/record_project/service/UserService.java
+++ src/main/java/com/changi/record_project/service/UserService.java
@@ -2,12 +2,14 @@ package com.changi.record_project.service;
 
 import java.sql.SQLException;
 
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
  
-//@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT)
-public interface MemberShipService {
+
+@Service
+public interface UserService {
 	
 //	void insertAndPointUpOfMember(NoticeVO notice, String id) throws ClassNotFoundException, SQLException ;
 
