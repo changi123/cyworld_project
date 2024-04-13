ff3d05f5d05839574269da3e9a0f47ff36772fb7
hcghcg17 <hcghcg17@naver.com>
2024-04-13 13:48:28 +0900

    Feat : 소켓 통신 완료

--- src/main/java/com/changi/record_project/base/WebSockChatHandler.java
+++ src/main/java/com/changi/record_project/base/WebSockChatHandler.java
@@ -38,6 +38,7 @@ public class WebSockChatHandler extends TextWebSocketHandler {
         ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
         Set<WebSocketSession> sessions=room.getSessions();   //방에 있는 현재 사용자 한명이 WebsocketSession
         if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
+        	
             //사용자가 방에 입장하면  Enter메세지를 보내도록 해놓음.  이건 새로운사용자가 socket 연결한 것이랑은 다름.
             //socket연결은 이 메세지 보내기전에 이미 되어있는 상태
             sessions.add(session);
