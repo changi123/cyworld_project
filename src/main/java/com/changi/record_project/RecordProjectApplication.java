ec7538ebcb0481dc576562426e0235416ec43f81
hcghcg17 <hcghcg17@naver.com>
2024-04-16 15:15:46 +0900

    Feat : 웹소켓 완료

--- src/main/java/com/changi/record_project/RecordProjectApplication.java
+++ src/main/java/com/changi/record_project/RecordProjectApplication.java
@@ -2,13 +2,21 @@ package com.changi.record_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RecordProjectApplication {
+public class RecordProjectApplication  extends SpringBootServletInitializer {
+
+
+	@Override 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { 
+		return application.sources(RecordProjectApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecordProjectApplication.class, args);
 		
 	}
-
 }
